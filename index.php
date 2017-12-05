d34287cf33cec719ec00ae3ea694e1d8c411202d
Cyteless <cmbunit01@gmail.com>
2017-12-05 16:19:02 +0000

    Revision 3 - Rewrite
    Rewrite version 3. Desktop-first design. Initial implementation.

---
+++ index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<head>
+  <title>VOLCBAT - Arma 3 Realism</title>
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" href="css/style.css" type="text/css">
+  <link rel="stylesheet" href="css/vcb-buttons.css" type="text/css">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Lato|Arimo|Josefin+Sans|Open+Sans+Condensed:400|Yellowtail" rel="stylesheet">
+
+</head>
+<body>
+
+  <nav class="nav-bar">
+    <div class="nav-box">
+      <a class="nav-logo" href="#">
+      </a>
+      <div class="nav-links">
+        <ul class="nav-buttons">
+          <li><a class="nav-link" href="#">Join</a></li>
+          <li><a class="nav-link" href="#">Media</a></li>
+          <li><a class="nav-link" href="#">ORBAT</a></li>
+          <li><a class="nav-link" href="#">News</a></li>
+          <li><a class="nav-link" href="#">Forums</a></li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <main class="main-content">
+
+    <header class="hero">
+      <div class="hero-box">
+        <h1 class="hero-text">
+          <span class="hero-text-main">VOLCBAT</span>
+          <span class="hero-text-sub">13 Years of Arma Realism Gaming</span>
+        </h1>
+        <div class="hero-button">
+          <a id="btn-join" class="btn btn-primary" href="#">Join</a>
+        </div>
+      </div>
+    </header>
+
+    <article class="home-content">
+      <div class="home-container">
+
+        <section class="home-intro">
+          <div class="home-intro-image">
+            <img src="images/badge.png" alt="">
+          </div>
+          <div class="home-intro-text">
+            <header>
+              <h2>The Volunteer Commandos</h2>
+            </header>
+            <main>
+              <p>
+                Formed in 2003, VOLCBAT provides a cooperative Arma 3 gameplay experience using authentic tactics employed by the UK Armed Forces.
+              </p>
+              <p>
+                We balance realism with fun, ensuring that each mission session provides tactical gameplay in a constructive environment.
+              </p>
+              <p class="home-intro-tagline">
+                Realism without the bollocks.
+              </p>
+            </main>
+          </div>
+        </section>
+
+        <section class="home-body">
+          <ul class="home-body-list">
+            <li>
+              <div class="home-body-list-image">
+                <img src="images/home/body/icon-training.png" alt="">
+              </div>
+              <div class="home-body-list-text">
+                <h2>Training</h2>
+                <h3>Recruits and Members</h3>
+                <p>
+                  Phase One trains infantry skills to new members in a 2 hour session.
+                  Platoon training is regularly run by current and former UK Armed Forces members, providing up to date and relevant tactics.
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="home-body-list-image">
+                <img src="images/home/body/icon-operations.png" alt="">
+              </div>
+              <div class="home-body-list-text">
+                <h2>Operations</h2>
+                <h3>8pm UK Time Wed/Sun</h3>
+                <p>
+                  We run in-house campaigns with varied missions, developed by members with our mission framework, scenario guides and templates.
+                </p>
+              </div>
+            </li>
+            <li>
+              <div class="home-body-list-image">
+                <img src="images/home/body/icon-addons.png" alt="">
+              </div>
+              <div class="home-body-list-text">
+                <h2>Addons</h2>
+                <h3>Steam Workshop</h3>
+                <p>
+                  Community addons and in-house mods come as an easy download, providing players and mission makers with all of the assets they need to play and create.
+                </p>
+              </div>
+            </li>
+          </ul>
+        </section>
+
+      </div>
+    </article>
+
+    <aside class="home-video">
+      <div class="home-video-container">
+        <main>
+          <div class="videoWrapper">
+            <iframe   width="560"
+                height="315"
+                src="https://www.youtube.com/embed/f7OO8QCV9Ys?rel=0&amp;showinfo=0;&amp;wmode=transparent"
+                frameborder="0"
+                allowfullscreen>
+            </iframe>
+          </div>
+        </main>
+      </div>
+    </aside>
+
+  </main>
+
+  <footer>
+
+    <div class="footer-container">
+      <header>
+        <h2>
+          <span class="footer-title">Volunteer Commando Battalion</span>
+          <span class="footer-subtitle">Realism without the bollocks</span>
+        </h2>
+      </header>
+
+      <p>Copyright VOLCBAT 2017</p>
+      <p>
+        This website is not affiliated or authorized by Bohemia
+        Interactive a.s. Bohemia Interactive, ARMA and all
+        associated logos and designs are trademarks or registered
+        trademarks of Bohemia Interactive a.s.
+      </p>
+
+      <ul class="social">
+        <li>Facebook</li>
+        <li>Twitter</li>
+        <li>YouTube</li>
+      </ul>
+
+      <p class="credit">Web design by Ben Ireland/Cyteless.</p>
+    </div>
+
+  </footer>
+
+</body>
